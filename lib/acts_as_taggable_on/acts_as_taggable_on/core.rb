f7748768c163ea4b3b1a27485aeab1a0aee46da5
Mike Wille <mike.wille@flowz.com>
2015-10-13 09:52:29 -0400

    Merged pull request https://github.com/mbleigh/acts-as-taggable-on/pull/583/files for working with Rails 4.2.

--- lib/acts_as_taggable_on/acts_as_taggable_on/core.rb
+++ lib/acts_as_taggable_on/acts_as_taggable_on/core.rb
@@ -324,10 +324,10 @@ module ActsAsTaggableOn::Taggable
       if changed_attributes.include?(attrib)
         # The attribute already has an unsaved change.
         old = changed_attributes[attrib]
-        changed_attributes.delete(attrib) if (old.to_s == value.to_s)
+        @changed_attributes.delete(attrib) if (old.to_s == value.to_s)
       else
         old = tag_list_on(context).to_s
-        changed_attributes[attrib] = old if (old.to_s != value.to_s)
+        @changed_attributes[attrib] = old if (old.to_s != value.to_s)
       end
     end
 
@@ -399,6 +399,21 @@ module ActsAsTaggableOn::Taggable
       end
 
       true
+    end
+
+    # Filters the tag lists from the attribute names.
+    def attributes_for_update(attribute_names)
+      filter_tag_lists(super)
+    end
+
+    # Filters the tag lists from the attribute names.
+    def attributes_for_create(attribute_names)
+      filter_tag_lists(super)
+    end
+
+    def filter_tag_lists(attributes)
+      tag_lists = tag_types.map {|tags_type| "#{tags_type.to_s.singularize}_list"}
+      attributes.delete_if {|attr| tag_lists.include? attr }
     end
   end
 end
